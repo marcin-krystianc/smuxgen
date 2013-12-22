9428ada593640cf9aa04b598eda48d4d3520b0ab
marcin.krystianc <marcin.krystianc@gmail.com>
2013-12-22 14:05:43 +0100

    build

--- build7z.bat
+++ build7z.bat
@@ -3,5 +3,5 @@ xcopy /D /Y "%~dp0\build-smuxgen-qt_4_8-Release\release\smuxgen.exe" "%~dp0\rele
 rmdir "%name%" /S /Q
 mkdir "%name%" 
 xcopy /D /Y /S "%~dp0\release" "%~dp0\\%name%"
-"C:\Program Files\7-Zip\7z.exe" a -t7z -mx9 "%name%.7z" "%name%\*"
+"C:\Program Files (x86)\7-Zip\7z.exe" a -t7z -mx9 "%name%.7z" "%name%\*"
 pause
